433f3aa01ee648a80fff82c875c6a995945d651e
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-05-29 00:29:01 +0530

    Stricted Type

---
+++ Coding Part/Day3_Type_Basic/sandbox.js
@@ -0,0 +1,14 @@
+var names = "aks";
+var age = 30;
+var isActive = true;
+//names=20
+names = "jks";
+//age="yteb"
+age = 38;
+//isActive="aj"
+isActive = false;
+var cir = function (diameter) {
+    return diameter * Math.PI;
+};
+//console.log(cir("ak"))-> only number will be passed 
+console.log(cir(2));
---
+++ Coding Part/Day3_Type_Basic/sandbox.ts
@@ -0,0 +1,21 @@
+let names="aks";
+let age=30;
+let isActive=true;
+
+
+//names=20
+names="jks"
+
+//age="yteb"
+age=38
+
+//isActive="aj"
+isActive=false;
+
+const cir=(diameter:number)=>{
+    return diameter*Math.PI;
+}
+
+//console.log(cir("ak"))-> only number will be passed 
+
+console.log(cir(2))
