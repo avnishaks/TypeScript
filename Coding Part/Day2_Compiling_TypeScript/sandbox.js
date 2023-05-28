2cacb3a808d80cb9c55f913507a2e95b9810217a
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-05-29 00:07:30 +0530

    Compling TypeScript

---
+++ Coding Part/Day2_Compiling_TypeScript/sandbox.js
@@ -0,0 +1,7 @@
+var variables = "avnishaks";
+console.log(variables);
+var input = document.querySelectorAll('input');
+console.log(input);
+input.forEach(function (input) {
+    console.log(input);
+});
---
+++ Coding Part/Day2_Compiling_TypeScript/sandbox.ts
@@ -0,0 +1,11 @@
+const variables="avnishaks"
+
+console.log(variables)
+
+const input=document.querySelectorAll('input')
+
+console.log(input)
+
+input.forEach(input=>{
+    console.log(input)
+})
