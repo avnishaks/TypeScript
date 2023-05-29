7704e0a0003e1320b3ac66a1596b6ef44db3e664
Avnish Kumar <avnish.kumar@ACKO880.local>
2023-05-29 13:55:33 +0530

    Dynamic Types

---
+++ Coding Part/Day6_Dynamic_Types/sandbox.js
@@ -0,0 +1,18 @@
+// Variables
+var value;
+value = "avish";
+console.log(value);
+value = 90;
+console.log(value);
+value = true;
+console.log(value);
+// Array
+var arr = [];
+arr.push(10);
+arr.push("avnush");
+arr.push(true);
+console.log(arr);
+// Object
+var ninja;
+ninja = { name: "Avnish", value: 35, islive: true };
+console.log(ninja);
---
+++ Coding Part/Day6_Dynamic_Types/sandbox.ts
@@ -0,0 +1,30 @@
+// Variables
+
+let value:any
+
+value="avish"
+console.log(value)
+
+value=90
+console.log(value)
+
+value=true
+console.log(value)
+
+
+// Array
+
+let arr:any[]=[]
+arr.push(10)
+arr.push("avnush")
+arr.push(true)
+console.log(arr)
+
+
+// Object
+
+let ninja:{name:any,value:any,islive:any}
+
+ninja={name:"Avnish",value:35,islive:true}
+
+console.log(ninja)
